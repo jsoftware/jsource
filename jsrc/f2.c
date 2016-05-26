cbf0be1feb98488e046a41e6f8b21a4d3ac69c4c
HenryHRich <HenryHRich@gmail.com>
2016-05-26 16:55:13 -0400

    Commentary for dyad ":

--- jsrc/f2.c
+++ jsrc/f2.c
@@ -137,53 +137,90 @@ static A jtth2a(J jt,B e,I m,I d,C*s,I n,I t,I wk,C*wv,B first){PROLOG;A y,z;B b
  EPILOG(y);
 }    /* like th2c, but allocates and returns array */
 
-static B jtth2ctrl(J jt,A a,A*ep,A*mp,A*dp,A*sp,I*zkp){A da,ea,ma,s;B b=1,*ev,r;
-  C*sv;D x,y;I an,*av,d,*dv,i,m,*mv,sk=15,zk=0;Z*au;
+// Analyze the x arg in x ": y
+// Result is 0 if x is well-formed, 1 if not
+// Output is 4 arrays and 1 number.  Arrays have 1 entry for each atom of a:
+// ep 1 if the result is in exponential form, 0 if decimal
+// mp total minimum field width
+// dp number of decimal places
+// sp sprintf format string to use for the field
+// zkp has the total field width (if known), or 0 if there were fields of unknown width
+// Also we create a conversion buffer area, pointed to by jt->th2buf, whose length
+// is jt->th2bufn (long enough to hold 1 1-cell of the result).  We calculate the needed
+// size here, by starting with a length of 500 and incrementing as required
+static B jtth2ctrl(J jt,A a,A*ep,A*mp,A*dp,A*sp,I*zkp){A da,ea,ma,s;B b=1,*ev,r,x;
+  C*sv;D x,y;I an,*av,d,*dv,i,m,*mv,const sk=15,zk=0;Z*au;
+ // b means 'all fields have a defined width'
+ // zk holds the total of the field sizes
+ // r='non-complex a', init length of conversion area to 500 bytes, convert a to int if it's not complex
  r=!(CMPX&AT(a)); jt->th2bufn=500;
- if(r)RZ(a=cvt(INT,a));
- an=AN(a); au=ZAV(a); av=AV(a);
- GA(ea,B01,an,   1,0); *ep=ea; ev=BAV(ea);
- GA(ma,INT,an,   1,0); *mp=ma; mv= AV(ma);
- GA(da,INT,an,   1,0); *dp=da; dv= AV(da);
- GA(s, LIT,an*sk,2,0); *sp=s;  sv=CAV(s); *AS(s)=an; *(1+AS(s))=sk;
+ if(r)RZ(a=cvt(INT,a));  // this detects invalid type for a
+ an=AN(a); au=ZAV(a); av=AV(a);  // an=#atoms of a, au->a data (if complex), av->a data (if real)
+ // Allocate output arrays, set return value, set ?v->first value of output area
+ GA(ea,B01,an,   1,0); *ep=ea; ev=BAV(ea);  // exponential flag, Boolean list
+ GA(ma,INT,an,   1,0); *mp=ma; mv= AV(ma);  // field width, integer list
+ GA(da,INT,an,   1,0); *dp=da; dv= AV(da);  // #decimal places, integer list
+ GA(s, LIT,an*sk,2,0); *sp=s;  sv=CAV(s); *AS(s)=an; *(1+AS(s))=sk;  // spritf string, set as nx(sk) table
+ // Look at each atom of a
  for(i=0;i<an;++i){
-  if(r){m=av[i]; x=(D)m; d=0;}
+  // Split a into field-width m and #decimal places d, and x as a flag, negative to indicate exponential form
+  if(r){m=av[i]; x=m<0; d=0;}  // real a: m= value of a, d=0 (no decimal places), 
   else{
-   x=au[i].re; m=(I)tfloor(x); ASSERT(teq(x,(D)m),EVDOMAIN);
-   y=au[i].im; d=(I)tfloor(y); ASSERT(teq(y,(D)d),EVDOMAIN); if(0>y)x=-1;
+   y=au[i].re; m=(I)tfloor(y); ASSERT(teq(y,(D)m),EVDOMAIN); x=m<0;  // real is field size, audit is integer
+   y=au[i].im; d=(I)tfloor(y); ASSERT(teq(y,(D)d),EVDOMAIN); if(0>y)x=1;  // imag is decimal places
   }
-  if(0>m)m=-m; if(0>d)d=-d; ASSERT(0<=m&&0<=d,EVLIMIT);
-  if(0<=x)sprintf(sv, "%%"FMTI"."FMTI"f",  m,d);
-  else    sprintf(sv, m?"%%- "FMTI"."FMTI"e" :"%%-"FMTI"."FMTI"e", m?m-1:0,d+!!(SYS&SYS_PC));
-  sv+=sk; ev[i]=0>x; mv[i]=m; dv[i]=d; zk+=m; b=b&&m; 
+  // Take abs of field sizes
+  if(0>m)m=-m; if(0>d)d=-d; ASSERT(0<=m&&0<=d,EVLIMIT);  // verify no overflow
+  // Create sprintf format string for the field, depending on decimal/exponential form
+  if(!x)sprintf(sv, "%%"FMTI"."FMTI"f",  m,d);  // %m.df
+  else    sprintf(sv, m?"%%- "FMTI"."FMTI"e" :"%%-"FMTI"."FMTI"e", m?m-1:0,d+!!(SYS&SYS_PC));  // %- m.de (m=0)  or %-m.de (m!=0)
+  // store results in output areas; advance sprintf pointer; count # bytes in fields; see if there are any unknown widths
+  sv+=sk; ev[i]=x; mv[i]=m; dv[i]=d; zk+=m; b=b&&m; 
+  // keep the size of the conversion buffer to a minimum of the given field width or 500+the number of decimal places (in case the values overflows the field)
   if(jt->th2bufn<m)jt->th2bufn=m; if(jt->th2bufn<500+d)jt->th2bufn=500+d;
  }
+ // Now that we know the conversion buffer size, allocate it
  GA(s,LIT,jt->th2bufn,1,0); jt->th2buf=CAV(s);
+ // Return total line width if it is valid, 0 if not
  *zkp=b?zk:0; R 1;
 }    /* parse format control (left argument of ":) */
 
+// x ": y
 F2(jtthorn2){PROLOG;A da,ea,h,ma,s,y,*yv,z;B e,*ev;C*sv,*wv,*zv;I an,c,d,*dv,k,m,*mv,n,r,sk,t,wk,*ws,zk;
- F2RANK(1,RMAX,jtthorn2,0);
- an=AN(a); t=AT(w);
- if(t&BOX)R th2box(a,w);
- ASSERT(t&NUMERIC&&!(t&SPARSE)&&!(AT(a)&SPARSE),EVDOMAIN);
+ F2RANK(1,RMAX,jtthorn2,0);  // apply rank 1 _
+ // From here on the a arg is rank 0 or 1
+ an=AN(a); t=AT(w);  // an=#atoms of a, t=type of w
+ if(t&BOX)R th2box(a,w);  // If boxed w, go handle as special case
+ ASSERT(t&NUMERIC&&!(t&SPARSE)&&!(AT(a)&SPARSE),EVDOMAIN);  // w must be numeric and dense; a must be dense
+ // r=rank of w; ws->shape of w; c=#atoms in 1-cell of w; n = #1-cells of w
  r=AR(w); ws=AS(w); c=r?ws[r-1]:1; n=c?AN(w)/c:prod(r-1,ws);
- ASSERT(!AR(a)||c==an,EVLENGTH);
+ ASSERT(!AR(a)||c==an,EVLENGTH);  // if a is not an atom, it must have the same length as a 1-cell of w
+ // k=#bytes in an atom of w, wk=*bytes in a cell of w; wv->first atom of w (prebiased)
  k=bp(t); wk=c*k; wv=CAV(w)-k;
+ // Analyze a to get info for each format
  RZ(th2ctrl(a,&ea,&ma,&da,&s,&zk));
+ // ev->expformat flags, mv->field width, dv->decimal places, sk=length of each sprintf string, sv->sprintf string (prebiased)
  ev=BAV(ea); mv=AV(ma); dv=AV(da); sk=1<an?*(1+AS(s)):0; sv=CAV(s)-sk;
  if(zk||!AN(w)){
-  if(1==an)zk*=c;
-  GA(z,LIT,n*zk,r?r:1,ws); *(AS(z)+AR(z)-1)=zk; zv=CAV(z);
-  DO(c, if(i<an){e=ev[i]; m=mv[i]; d=dv[i];} th2c(e,m,d,sv+=sk,n,t,wk,wv+=k,zk,zv); zv+=m;);
+  // We know the width, or there is nothing to format.  Create the lines one by one
+  if(1==an)zk*=c;   // If only one atom in a, replicate it to match a line of w
+  GA(z,LIT,n*zk,r?r:1,ws); *(AS(z)+AR(z)-1)=zk; zv=CAV(z);  // Allocate table for result; init shape to shape of w; replace 1-cell length with length of line; zv->result area
+  // Format the fields one by one, appending the new string to the accumulated old.  We process each field specifier for the entire
+  // w before moving to the next field.
+  DO(c, if(i<an){e=ev[i]; m=mv[i]; d=dv[i];} th2c(e,m,d,sv+=sk,n,t,wk,wv+=k,zk,zv); zv+=m;);  // Set e,m,d (if atomic a, keep the same values each time
  }else{
-  GA(y,BOX,c,1,0); yv=AAV(y);
+  // The width is unknown.  Build up the result one field at a time.  Each field becomes a box in this intermediate result
+  GA(y,BOX,c,1,0); yv=AAV(y);  // Allocate boxed array, one box per field
+  // Format each field into its own box
   DO(c, if(i<an){e=ev[i]; m=mv[i]; d=dv[i];} RZ(yv[i]=th2a(e,m,d,sv+=sk,n,t,wk,wv+=k,(B)!i)););
+  // Join the fields of each line to produce an nxc table of characters, one row per 1-cell of w
   RZ(z=razeh(y));
+  // If w has rank > 2, we need to rearrange the rows into an array.  Or, if there is a single 1-cell and
+  // r was an atom or a list, we need to change the result from a table to a single list.
   if(2<r||1==n&&2!=r){
-   if(!r)r=1; 
-   RZ(h=vec(INT,r,ws)); *(AV(h)+r-1)=*(1+AS(z)); 
-   RZ(z=reshape(h,z));
+   if(!r)r=1;   // change atomic r to a list
+   RZ(h=vec(INT,r,ws)); *(AV(h)+r-1)=*(1+AS(z));  // Create a vector out of the shape of w; replace length of 1-cell with length of a row of result 
+   RZ(z=reshape(h,z));   // reshape the result table to that shape
  }}
  EPILOG(z);
 }

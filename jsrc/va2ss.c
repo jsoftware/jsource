ece6642df763123152e791901482a566cff87bd4
Eric Iverson <eiverson@jsoftware.com>
2016-07-04 09:51:24 -0400

    va2ss.c lowercase

--- jsrc/va2ss.c
+++ jsrc/va2ss.c
@@ -1,4 +1,4 @@
-/* Copyright 1990-2011, Jsoftware Inc.  All rights reserved.               */
+/* Copyright 1990-2016, Jsoftware Inc.  All rights reserved.               */
 /* Licensed use only. Any other use is in violation of copyright.          */
 /*                                                                         */
 // Verbs: Atomic (Scalar) Dyadic when arguments have one atom

5b0406c02ac981bbdd3e1d42444d5e28b0ae3eb9
bill lam <bbill.lam@gmail.com>
2017-04-22 15:43:34 +0800

    android mk enable neon support

--- jsrc/Jconsole-nopie.mk
+++ jsrc/Jconsole-nopie.mk
@@ -10,6 +10,7 @@ LOCAL_LDLIBS := -ldl -lc
 ifeq ($(TARGET_ARCH_ABI),arm64-v8a)
   LOCAL_CFLAGS := -fPIC -Os -Werror -fno-strict-aliasing -DREADLINE -march=armv8-a
   LOCAL_ARM_MODE := arm
+  LOCAL_ARM_NEON := true
 endif
 ifeq ($(TARGET_ARCH_ABI),armeabi-v7a)
   LOCAL_CFLAGS := -fPIC -Os -Werror -fno-strict-aliasing -DREADLINE -mfloat-abi=softfp -mfpu=vfpv3-d16 -march=armv7-a
@@ -23,7 +24,7 @@ ifeq ($(TARGET_ARCH),x86_64)
   LOCAL_CFLAGS := -fPIC -Os -Werror -fno-strict-aliasing -DREADLINE -march=x86-64 -mtune=intel -msse4.2 -mpopcnt -fno-stack-protector
 endif
 ifeq ($(TARGET_ARCH),x86)
-  LOCAL_CFLAGS := -fPIC -Os -Werror -fno-strict-aliasing -DREADLINE -march=i686 -mtune=intel -mssse3 -mfpmath=sse -fno-stack-protector
+  LOCAL_CFLAGS := -fPIC -Os -Werror -fno-strict-aliasing -DREADLINE -march=i686 -mtune=intel -msse2 -mfpmath=sse -fno-stack-protector
 endif
 
 LOCAL_SRC_FILES :=  jconsole.c jeload.c linenoise.c
--- jsrc/Tsdll.mk
+++ jsrc/Tsdll.mk
@@ -11,6 +11,7 @@ LOCAL_LDLIBS := -lc
 ifeq ($(TARGET_ARCH_ABI),arm64-v8a)
   LOCAL_CFLAGS := -fPIC -Os -Werror -fno-strict-aliasing -march=armv8-a
   LOCAL_ARM_MODE := arm
+  LOCAL_ARM_NEON := true
 endif
 ifeq ($(TARGET_ARCH_ABI),armeabi-v7a)
   LOCAL_CFLAGS := -fPIC -Os -Werror -fno-strict-aliasing -mfloat-abi=softfp -mfpu=vfpv3-d16 -march=armv7-a
@@ -24,7 +25,7 @@ ifeq ($(TARGET_ARCH),x86_64)
   LOCAL_CFLAGS := -fPIC -Os -Werror -fno-strict-aliasing -march=x86-64 -mtune=intel -msse4.2 -mpopcnt -fno-stack-protector
 endif
 ifeq ($(TARGET_ARCH),x86)
-  LOCAL_CFLAGS := -fPIC -Os -Werror -fno-strict-aliasing -march=i686 -mtune=intel -mssse3 -mfpmath=sse -fno-stack-protector
+  LOCAL_CFLAGS := -fPIC -Os -Werror -fno-strict-aliasing -march=i686 -mtune=intel -msse2 -mfpmath=sse -fno-stack-protector
 endif
 
 LOCAL_SRC_FILES :=  tsdll.c

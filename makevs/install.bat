16d4c358ab9d031cd75ac5770af8d992a6e36bf8
Eric Iverson <eiverson@jsoftware.com>
2018-02-22 14:57:54 -0500

    windows 807 release

--- makevs/install.bat
+++ makevs/install.bat
@@ -1,12 +1,13 @@
-rem %1% is folder with jsource
-
+rem run in home
 rem create jbld folders for j32 and j64
+
+cd %userprofile%
 
 rmdir /S /Q jbld
 
 mkdir jbld\j32
 mkdir jbld\j64
 
-xcopy /E /Y /Q %1%\jsource\jlibrary jbld\j32\
-xcopy /E /Y /Q %1%\jsource\jlibrary jbld\j64\
+xcopy /E /Y /Q gitdev\jsource\jlibrary jbld\j32\
+xcopy /E /Y /Q gitdev\jsource\jlibrary jbld\j64\
     

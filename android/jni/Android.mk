15c38219610768822232e9e31eb15d12f7e29c25
bill lam <bbill.lam@gmail.com>
2018-02-24 11:54:18 +0800

    android

--- android/jni/Android.mk
+++ android/jni/Android.mk
@@ -9,6 +9,7 @@ include jni/jsrc/Jconsole.mk
 include jni/jsrc/Jconsole-nopie.mk
 include jni/jsrc/Tsdll.mk
 include jni/pcre/Android.mk
+include jni/pcre2/Android.mk
 include jni/hostdefs/Hostdefs.mk
 include jni/hostdefs/Hostdefs-nopie.mk
 include jni/netdefs/Netdefs.mk

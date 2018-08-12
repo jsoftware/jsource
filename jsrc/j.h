2a9c6c7827c650090d0192584393352b31cc9c62
bill lam <bbill.lam@gmail.com>
2018-08-12 12:41:32 +0800

    reverting Try adding CCBLOCK to make clang work

--- jsrc/j.h
+++ jsrc/j.h
@@ -526,6 +526,25 @@ extern unsigned int __cdecl _clearfp (void);
 #define MLEN            (SY_64?63:31)
 #define MODVIRTINPLACE(x)  (AFLAG(x) |= (AFLAG(x)&AFVIRTUAL)<<(AFVIRTUALINPLACEX-AFVIRTUALX))
 
+#ifdef __STDC_IEC_559__
+#include <fenv.h>
+#define NAN0 feclearexcept(FE_ALL_EXCEPT)
+#define NANTEST (fetestexcept(FE_INVALID))
+#else
+#define NAN0            (_clearfp())
+#if defined(_MSC_VER) && _MSC_VER==1800 && !SY_64 // bug in some versions of VS 2013
+// obsolete #define NAN1            {if(_SW_INVALID&_statusfp()){NAN0;jsignal(EVNAN); R 0;}}
+// obsolete #define NAN1V           {if(_SW_INVALID&_statusfp()){NAN0;jsignal(EVNAN); R  ;}}
+#define NANTEST         (_SW_INVALID&_statusfp())
+#else
+// obsolete #define NAN1            {if(_SW_INVALID&_clearfp()){jsignal(EVNAN); R 0;}}
+// obsolete #define NAN1V           {if(_SW_INVALID&_clearfp()){jsignal(EVNAN); R  ;}}
+#define NANTEST         (_SW_INVALID&_clearfp())
+#endif
+#endif
+#define NAN1            {if(NANTEST){NAN0;jsignal(EVNAN); R 0;}}
+#define NAN1V           {if(NANTEST){NAN0;jsignal(EVNAN); R  ;}}
+
 #define NUMMIN          (-9)    // smallest number represented in num[]
 #define NUMMAX          9    // largest number represented in num[]
 // PROD multiplies a list of numbers, where the product is known not to overflow a signed int (for example, it might be part of the shape of a dense array)
@@ -810,10 +829,8 @@ extern J gjt; // global for JPF (procs without jt)
 /* (hopefully) turn off some re-scheduling optimization  */
 #ifdef __GNUC__
 #define CCBLOCK __asm__("" ::: "cc")
-#define CCBLOCKC CCBLOCK,
 #else
 #define CCBLOCK
-#define CCBLOCKC
 #endif
 
 #if SYS & SYS_UNIX
@@ -953,23 +970,3 @@ static __forceinline void aligned_free(void *ptr) {
 #endif
 #define CRC32LL CRC32L                 // takes UIL (8 bytes), return UI
 #endif
-
-
-#ifdef __STDC_IEC_559__
-#include <fenv.h>
-#define NAN0 CCBLOCK;feclearexcept(FE_ALL_EXCEPT);CCBLOCK
-#define NANTEST (CCBLOCKC fetestexcept(FE_INVALID))
-#else
-#define NAN0            CCBLOCK;(_clearfp());CCBLOCK
-#if defined(_MSC_VER) && _MSC_VER==1800 && !SY_64 // bug in some versions of VS 2013
-// obsolete #define NAN1            {if(_SW_INVALID&_statusfp()){NAN0;jsignal(EVNAN); R 0;}}
-// obsolete #define NAN1V           {if(_SW_INVALID&_statusfp()){NAN0;jsignal(EVNAN); R  ;}}
-#define NANTEST         (CCBLOCKC _SW_INVALID&_statusfp())
-#else
-// obsolete #define NAN1            {if(_SW_INVALID&_clearfp()){jsignal(EVNAN); R 0;}}
-// obsolete #define NAN1V           {if(_SW_INVALID&_clearfp()){jsignal(EVNAN); R  ;}}
-#define NANTEST         (CCBLOCKC _SW_INVALID&_clearfp())
-#endif
-#endif
-#define NAN1            {if(NANTEST){NAN0;jsignal(EVNAN); R 0;}}
-#define NAN1V           {if(NANTEST){NAN0;jsignal(EVNAN); R  ;}}

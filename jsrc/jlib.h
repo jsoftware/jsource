f00a4ea1481a00a87e06dcc2006fe694d43ffde3
bill lam <bbill.lam@gmail.com>
2018-10-06 08:50:31 +0800

    JSMX JGetR cleanup

--- jsrc/jlib.h
+++ jsrc/jlib.h
@@ -3,22 +3,24 @@
 
 J _stdcall JInit();                         /* init instance */
 void _stdcall JSM(J jt, void*callbacks[]);  /* set callbacks */
-void _stdcall JSMX(J jt, void*, void*, void*, void*, I);  
+void _stdcall JSMX(J jt, void*, void*, void*, void*, I);
 int _stdcall JDo(J jt,C*);                  /* run sentence */
-C* _stdcall JGetLocale(J jt);               /* get locale */   
+C* _stdcall JGetLocale(J jt);               /* get locale */
 A _stdcall Jga(J jt, I t, I n, I r, I*s);
 int _stdcall JFree(J jt);                   /* free instance */
 A _stdcall JGetA(J jt,I n,C* name);         /* get 3!:1 from name */
+C* _stdcall JGetR(J jt);                    /* get capture */
 I _stdcall JSetA(J jt,I n,C* name,I x,C* d);/* name=:3!:2 data */
 int _stdcall JGetM(J jt, C* name, I* jtype, I* jrank, I* jshape, I* jdata);
 int _stdcall JSetM(J jt, C* name, I* jtype, I* jrank, I* jshape, I* jdata);
 int _stdcall JErrorTextM(J jt, I ec, I* p);
 
 typedef void* (_stdcall *JInitType)     ();
-typedef int   (_stdcall *JDoType)       (void*, C*);
-typedef C*    (_stdcall *JGetLocaleType)(void*);    
-typedef void  (_stdcall *JSMType)       (void*, void*);
-typedef void  (_stdcall *JFreeType)     (void*);
+typedef int   (_stdcall *JDoType)       (J, C*);
+typedef C*    (_stdcall *JGetLocaleType)(J);
+typedef void  (_stdcall *JSMType)       (J, void*);
+typedef void  (_stdcall *JSMXType)      (J, void*, void*, void*, void*, I);
+typedef void  (_stdcall *JFreeType)     (J);
 typedef A     (_stdcall *JgaType)       (J jt, I t, I n, I r, I*s);
 typedef A     (_stdcall *JGetAType)     (J jt,I n,C* name);
 

fae646d5b7d68830e719aa3b508e03adcdc7c443
HenryHRich <HenryHRich@gmail.com>
2018-12-07 18:54:45 -0500

    Fix missing parens in linear rep of    noun-phrase 1 : m

--- jsrc/rl.c
+++ jsrc/rl.c
@@ -242,14 +242,15 @@ static B laa(A a,A w){C c,d;
 
 static B lnn(A a,A w){C c; if(!(a&&w))R 0; c=cl(a); R ('x'==c||C9==ctype[(UC)c])&&C9==ctype[(UC)cf(w)];}
 
-static F2(jtlinsert){A*av,f,g,h,t,t0,t1,t2,*u,y;B b,ft,gt,ht,vb;C c,id;I n;V*v;
+static F2(jtlinsert){A*av,f,g,h,t,t0,t1,t2,*u,y;B b,ft,gt,ht;C c,id;I n;V*v;
  RZ(a&&w);
  n=AN(a); av=AAV(a);  
- vb=VERB==AT(w); v=VAV(w); id=v->id;
+ v=VAV(w); id=v->id;
  b=id==CCOLON&&VXOP&v->flag;
- if(1<=n){f=av[0]; t=v->fgh[0]; c=ID(t); ft=c==CHOOK||c==CFORK||c==CADVF||id==CFORK&&NOUN&AT(t)&&lp(f);}
- if(2<=n){g=av[1]; t=v->fgh[1]; c=ID(t); gt=vb    ?c==CHOOK||c==CFORK:lp(g);}
- if(3<=n){h=av[2]; t=v->fgh[2]; c=ID(t); ht=vb&&!b?c==CHOOK          :lp(h);}
+// ?t tells whether () is needed around the f/g/h component
+ if(1<=n){f=av[0]; t=v->fgh[0]; c=ID(t); ft=c==CHOOK||c==CFORK||c==CADVF||(b||id==CFORK)&&NOUN&AT(t)&&lp(f);}  // f: () is it's hoor fork && or noun left end of nvv or n (op)
+ if(2<=n){g=av[1]; t=v->fgh[1]; c=ID(t); gt=VERB&AT(w)    ?c==CHOOK||c==CFORK:lp(g);}
+ if(3<=n){h=av[2]; t=v->fgh[2]; c=ID(t); ht=VERB&AT(w)&&!b?c==CHOOK          :lp(h);}
  switch(!b?id:2==n?CHOOK:CFORK){
   case CADVF:
   case CHOOK:

f4e000ab71b7e737609ae2b6ac9d735623cd6502
bill lam <bbill.lam@gmail.com>
2019-03-09 16:33:28 +0800

    option to build jconsole with linenoise

--- make/build_jconsole.sh
+++ make/build_jconsole.sh
@@ -9,20 +9,44 @@ macmin="-mmacosx-version-min=10.6"
 case $jplatform\_$1 in
 
 linux_j32)
+if [ -z "$USE_LINENOISE" ] ; then
 COMPILE="$common -m32 -DREADLINE"
 LINK=" -l:libedit.so.2 -m32 -ldl -o jconsole "
+else
+COMPILE="$common -m32 -DREADLINE -DUSE_LINENOISE"
+LINK=" -m32 -ldl -o jconsole "
+OBJSLN="linenoise.o"
+fi
 ;;
 linux_j64)
+if [ -z "$USE_LINENOISE" ] ; then
 COMPILE="$common -DREADLINE"
 LINK=" -ledit -ldl -o jconsole "
+else
+COMPILE="$common -DREADLINE -DUSE_LINENOISE"
+LINK=" -ldl -o jconsole "
+OBJSLN="linenoise.o"
+fi
 ;;
 raspberry_j32)
+if [ -z "$USE_LINENOISE" ] ; then
 COMPILE="$common -marm -march=armv6 -mfloat-abi=hard -mfpu=vfp -DREADLINE -DRASPI"
 LINK=" -ledit -ldl -o jconsole "
+else
+COMPILE="$common -marm -march=armv6 -mfloat-abi=hard -mfpu=vfp -DREADLINE -DUSE_LINENOISE -DRASPI"
+LINK=" -ldl -o jconsole "
+OBJSLN="linenoise.o"
+fi
 ;;
 raspberry_j64)
+if [ -z "$USE_LINENOISE" ] ; then
 COMPILE="$common -march=armv8-a+crc -DREADLINE -DRASPI"
 LINK=" -ledit -ldl -o jconsole "
+else
+COMPILE="$common -march=armv8-a+crc -DREADLINE -DUSE_LINENOISE -DRASPI"
+LINK=" -ldl -o jconsole "
+OBJSLN="linenoise.o"
+fi
 ;;
 darwin_j32)
 COMPILE="$common -m32 -DREADLINE $macmin"
@@ -38,7 +62,7 @@ echo no case for those parameters
 exit
 esac
 
-OBJS="jconsole.o jeload.o "
+OBJS="jconsole.o jeload.o ${OBJSLN}"
 TARGET=jconsole
 export OBJS COMPILE LINK TARGET
 $jmake/domake.sh $1

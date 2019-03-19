38c3a582945cc27e424fc90bdeb1882a33d406bd
Eric Iverson <eiverson@jsoftware.com>
2019-03-19 14:09:24 -0400

    build only j64 avx

--- make/build_all.sh
+++ make/build_all.sh
@@ -4,20 +4,9 @@ cd ~
 
 $jmake/install.sh
 
-if [ $jplatform != "darwin" ]; then
-$jmake/build_jconsole.sh j32
-$jmake/build_libj.sh     j32
-$jmake/build_tsdll.sh    j32
-fi
-
-# if [ $jplatform = "raspberry" ]; then
-#   exit 0
-# fi  
-
 $jmake/build_jconsole.sh j64
 $jmake/build_libj.sh     j64
 $jmake/build_tsdll.sh    j64
 
-$jmake/build_libj.sh     j64nonavx
 
 

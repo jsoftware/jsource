724713ebcd84f0eb622a0011a985787abb90dc5d
bill lam <bbill.lam@gmail.com>
2019-05-27 12:42:00 +0800

    make2 linux

--- make2/build_jconsolenr.sh
+++ make2/build_jconsolenr.sh
@@ -3,13 +3,13 @@
 realpath()
 {
  oldpath=`pwd`
- if ! cd $1 &> /dev/null; then
-  cd ${1##*/} &> /dev/null
+ if ! cd $1 > /dev/null 2>&1; then
+  cd ${1##*/} > /dev/null 2>&1
   echo $( pwd -P )/${1%/*}
  else
   pwd -P
  fi
- cd $oldpath > /dev/null
+ cd $oldpath > /dev/null 2>&1
 }
 
 cd "$(realpath "$0")"

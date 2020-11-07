b3f74fef4f8d60a061baf33f19d55fe6d12a8c00
bill lam <bbill.lam@gmail.com>
2020-11-07 09:43:57 +0800

    Fix Linux compile warning

--- jsrc/sn.c
+++ jsrc/sn.c
@@ -235,7 +235,7 @@ F1(jtex){A*wv,y,z;B*zv;I i,n;L*v;I modifierchg=0;
     if(!(AFLAG(v->val)&AFNVR)){
      // The symbol is a local symbol not on the NVR stack.  We must put it onto the NVR stack.
      A nvra=jt->nvra;
-     if(unlikely((jt->parserstackframe.nvrtop+1U) > AN(nvra)))RZ(nvra=extnvr());  // Extend nvr stack if necessary.  copied from parser
+     if(unlikely((I)(jt->parserstackframe.nvrtop+1U) > AN(nvra)))RZ(nvra=extnvr());  // Extend nvr stack if necessary.  copied from parser
      AAV1(nvra)[jt->parserstackframe.nvrtop++] = v->val;   // record the place where the value was protected; it will be freed when this sentence finishes
      AFLAG(v->val) |= AFNVR|AFNVRUNFREED;  // mark the value as protected
     }

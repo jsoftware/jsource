cc830e064205c24b5e96cbf0d41be480994a40a7
bill lam <bbill.lam@gmail.com>
2021-09-10 10:43:25 +0800

    update sse2neon

--- android/jni/Application.mk
+++ android/jni/Application.mk
@@ -5,7 +5,8 @@ APP_MODULES := libj jnative jconsole jconsole-nopie libtsdll libjpcre libjpcre2 
 # APP_MODULES := blas libf2c cholrl lapack
 # use ndk r14 gcc-4.9 to build armeabi and armeabi-v7a
 # APP_ABI := armeabi
-APP_ABI := armeabi-v7a x86 x86_64 arm64-v8a
+# APP_ABI := armeabi-v7a x86 x86_64 arm64-v8a
+APP_ABI := arm64-v8a
 # will be enable inside jconsole, it is easier to enable than disable
 APP_PIE := false
 # android 4.1

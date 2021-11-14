379ed0d2c5577fdfc980338b2407935357e72922
bill lam <bbill.lam@gmail.com>
2021-11-14 22:54:02 +0800

    test buildga.sh

--- script/buildga.sh
+++ script/buildga.sh
@@ -10,6 +10,7 @@ elif [ "$1" == "darwin" ]; then
   ext="dylib"
   xcmin="12"
   xcver=`xcodebuild -version|head -n1|cut -f2 -d" "|cut -f1 -d"."`
+  echo "xcver = $xcver"
 else
   echo "argument is linux|darwin"
   exit 1
@@ -34,6 +35,7 @@ j64x=j64 ./build_jconsole.sh
 j64x=j64 ./build_tsdll.sh
 j64x=j64 ./build_libj.sh
 if [ "$1" == "darwin" ] && [ "$xcver" -ge "$xcmin" ]; then
+echo "build arm64"
 ./clean.sh
 j64x=j64arm ./build_jconsole.sh
 j64x=j64arm ./build_tsdll.sh
@@ -47,6 +49,7 @@ j64x=j64avx2 ./build_libj.sh
 cd ..
 cp bin/$1/j64/* j64
 if [ "$1" == "darwin" ] && [ "$xcver" -ge "$xcmin" ]; then
+echo "build fat binary"
 lipo bin/$1/j64/jconsole bin/$1/j64arm/jconsole -create -output j64/jconsole
 lipo bin/$1/j64/libtsdll.$ext bin/$1/j64arm/libtsdll.$ext -create -output j64/libtsdll.$ext
 lipo bin/$1/j64/libj.$ext bin/$1/j64arm/libj.$ext -create -output j64/libj.$ext

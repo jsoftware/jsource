a891653196b8192101f9e35c9a4ca948837da396
bill lam <bbill.lam@gmail.com>
2022-06-26 14:09:06 +0800

    github windows clang-cl

--- base64/lib/codec_choose.c
+++ base64/lib/codec_choose.c
@@ -32,13 +32,15 @@
 #else
 	#include <cpuid.h>
 	#if HAVE_AVX2 || HAVE_AVX
-		#if !defined(_xgetbv) && ((__GNUC__ > 4 || __GNUC__ == 4 && __GNUC_MINOR__ >= 2) || (__clang_major__ >= 3))
+		#if ((__GNUC__ > 4 || __GNUC__ == 4 && __GNUC_MINOR__ >= 2) || (__clang_major__ >= 3))
+		#if !__has_builtin(_xgetbv)
 			static inline uint64_t _xgetbv (uint32_t index)
 			{
 				uint32_t eax, edx;
 				__asm__ __volatile__("xgetbv" : "=a"(eax), "=d"(edx) : "c"(index));
 				return ((uint64_t)edx << 32) | eax;
 			}
+		#endif
 		#else
 			#error "Platform not supported"
 		#endif

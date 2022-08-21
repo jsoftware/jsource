73c78227ab541cec8fa134510aed2662d5eadec1
Moonchild <elronnd@elronnd.net>
2022-08-21 11:26:24 +0000

    Revert "makevs/android/etc. -fwrapv"
    This reverts commit 11de76d12878b5fbaecb74122ccd24cba0db20f6.

--- hostdefs/Hostdefs.mk
+++ hostdefs/Hostdefs.mk
@@ -8,22 +8,22 @@ LOCAL_MODULE    := hostdefs
 LOCAL_LDLIBS := -ldl
 
 ifeq ($(TARGET_ARCH_ABI),arm64-v8a)
-  LOCAL_CFLAGS := -fPIC -O1 -fwrapv -Werror -fno-strict-aliasing -fwrapv -march=armv8-a
+  LOCAL_CFLAGS := -fPIC -O1 -fwrapv -Werror -fno-strict-aliasing -march=armv8-a
   LOCAL_ARM_NEON := true
 endif
 ifeq ($(TARGET_ARCH_ABI),armeabi-v7a)
-  LOCAL_CFLAGS := -fPIC -O1 -fwrapv -Werror -fno-strict-aliasing -fwrapv -mfloat-abi=softfp -mfpu=vfpv3-d16 -march=armv7-a
+  LOCAL_CFLAGS := -fPIC -O1 -fwrapv -Werror -fno-strict-aliasing -mfloat-abi=softfp -mfpu=vfpv3-d16 -march=armv7-a
   LOCAL_ARM_MODE := arm
 endif
 ifeq ($(TARGET_ARCH_ABI),armeabi)
-  LOCAL_CFLAGS := -fPIC -Os -fwrapv -Werror -fno-strict-aliasing -fwrapv -mfloat-abi=softfp -mfpu=vfp -march=armv5te
+  LOCAL_CFLAGS := -fPIC -Os -fwrapv -Werror -fno-strict-aliasing -mfloat-abi=softfp -mfpu=vfp -march=armv5te
   LOCAL_ARM_MODE := arm
 endif
 ifeq ($(TARGET_ARCH),x86_64)
-  LOCAL_CFLAGS := -fPIC -O1 -fwrapv -Werror -fno-strict-aliasing -fwrapv -msse4.2 -mpopcnt -fno-stack-protector
+  LOCAL_CFLAGS := -fPIC -O1 -fwrapv -Werror -fno-strict-aliasing -msse4.2 -mpopcnt -fno-stack-protector
 endif
 ifeq ($(TARGET_ARCH),x86)
-  LOCAL_CFLAGS := -fPIC -O1 -fwrapv -Werror -fno-strict-aliasing -fwrapv -mssse3 -mfpmath=sse -fno-stack-protector
+  LOCAL_CFLAGS := -fPIC -O1 -fwrapv -Werror -fno-strict-aliasing -mssse3 -mfpmath=sse -fno-stack-protector
 endif
 
 LOCAL_CFLAGS += -fPIE

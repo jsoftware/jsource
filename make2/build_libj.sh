30651bbcf21375cda25129e9c89976ff6249cb7c
Moonchild <elronnd@elronnd.net>
2022-09-16 00:11:07 -0700

    can set MAKEFLAGS to empty to disable parallelisation

--- make2/build_libj.sh
+++ make2/build_libj.sh
@@ -600,7 +600,7 @@ mkdir -p obj/$jplatform/$j64x/
 cp makefile-libj obj/$jplatform/$j64x/.
 export CFLAGS LDFLAGS TARGET CFLAGS_SIMD GASM_FLAGS FLAGS_SLEEF FLAGS_BASE64 DLLOBJS LIBJDEF LIBJRES OBJS_BASE64 OBJS_FMA OBJS_AESNI OBJS_AESARM OBJS_SLEEF OBJS_ASM SRC_ASM jplatform j64x
 cd obj/$jplatform/$j64x/
-if [ "x$MAKEFLAGS" = x'' ] ; then
+if [ ! -v MAKEFLAGS ] ; then
 if [ `uname` = Linux ]; then par=`nproc`; else par=`sysctl -n hw.ncpu`; fi
 make -j$par -f makefile-libj
 else

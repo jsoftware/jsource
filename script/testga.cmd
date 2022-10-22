f45161df3713405c52d7f3b2b02efca4f00b804b
bill lam <bbill.lam@gmail.com>
2022-10-22 15:31:27 +0800

    Revert "test windows"
    This reverts commit d1351884184f424c9e4cbc157017413f21938001.

--- script/testga.cmd
+++ script/testga.cmd
@@ -4,8 +4,8 @@ dir j64
 
 j64\jconsole -lib j.dll testga.ijs
 IF %ERRORLEVEL% NEQ 0 EXIT /b 1
-@rem j64\jconsole -lib javx.dll testga.ijs
-@rem IF %ERRORLEVEL% NEQ 0 EXIT /b 1
-@rem j64\jconsole -lib javx2.dll testga.ijs
-@rem IF %ERRORLEVEL% NEQ 0 EXIT /b 1
+j64\jconsole -lib javx.dll testga.ijs
+IF %ERRORLEVEL% NEQ 0 EXIT /b 1
+j64\jconsole -lib javx2.dll testga.ijs
+IF %ERRORLEVEL% NEQ 0 EXIT /b 1
 

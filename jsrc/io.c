fe2cef8d65eabb74c61370f156c7b7683bbe22da
Henry Rich <henryhrich@gmail.com>
2022-11-10 14:44:03 -0500

    Keep RUNNING task state until iep finishes; revise interface to eformat_j_

--- jsrc/io.c
+++ jsrc/io.c
@@ -400,10 +400,10 @@ static I jdo(JS jt, C* lp){I e;A x;JJ jm=MTHREAD(jt);  // get address of thread 
  // Check for DDs in the input sentence.  If there is one, call jgets() to finish it.  Result is enqueue()d sentence.  If recursive, don't allow call to jgets()
  x=jtddtokens(jm,x,(((jm->recurstate&RECSTATEPROMPT)<<(2-1)))+1+(AN(jm->locsyms)>SYMLINFOSIZE)); if(!jm->jerr)jtimmexexecct(jm,x);  // allow reads from jgets() if not recursive; return enqueue() result
  e=jm->jerr; if(savcallstack==0)CALLSTACKRESET(jm) MODESRESET(jm)  // save error on sentence to be our return code
- if(likely(wasidle))jtclrtaskrunning(jm);  //  when we finally return to FE, clear running state in case other tasks are running and need system lock - but not if recursion
  jtshowerr(jm);   // jt flags=0 to force typeout of iep errors
  RESETERRT(jm)
  if(likely(jm->recurstate<RECSTATEPROMPT))runiep(jt,jm,old,savcallstack);  // IEP does not display its errors
+ if(likely(wasidle))jtclrtaskrunning(jm);  //  when we finally return to FE, clear running state in case other tasks are running and need system lock - but not if recursion
  jtspfree(jm);
  jttpop(jm,old);
  R e;

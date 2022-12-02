e52c0cc9a5b7daccaed9de5ed694ab0d1661b7f7
bill lam <bbill.lam@gmail.com>
2022-12-02 11:10:46 +0800

    android build

--- android/jni/Android.mk
+++ android/jni/Android.mk
@@ -8,10 +8,10 @@ include jni/jsrc/Jnative.mk
 include jni/jsrc/Jconsole.mk
 include jni/jsrc/Jconsole-nopie.mk
 include jni/jsrc/Tsdll.mk
-include jni/pcre/Android.mk
-include jni/pcre2/Android.mk
 include jni/hostdefs/Hostdefs.mk
 include jni/hostdefs/Hostdefs-nopie.mk
 include jni/netdefs/Netdefs.mk
 include jni/netdefs/Netdefs-nopie.mk
+# include jni/pcre/Android.mk
+# include jni/pcre2/Android.mk
 # include jni/clapack/Android.mk

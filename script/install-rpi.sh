a1995b7ad33e0677c833a7c4341cb0e69b7f8ac6
bill lam <bbill.lam@gmail.com>
2022-12-03 17:16:06 +0800

    Revert "raspberry runner"
    This reverts commit 5569b1d641b9a6eb25f28f36ddf28d610461defd.

--- script/install-rpi.sh
+++ script/install-rpi.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
-set -e
-cd
 apt-get update -y --allow-releaseinfo-change
 apt-get install -y build-essential clang wget
 apt-get install -y nasm
-git clone https://github.com/jsoftware/jsource.git
-cd jsource
-script/buildga.sh raspberry
-script/testga.sh raspberry

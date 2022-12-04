4450bd76595da94630f82b68238a6efefad93f0f
bill lam <bbill.lam@gmail.com>
2022-12-04 14:52:50 +0800

    test

--- make2/jplatform64.sh
+++ make2/jplatform64.sh
@@ -5,19 +5,19 @@ set -vex
 
 cd "$(dirname "$0")"
 
-if [ "`uname`" != "Darwin" ] && ( [ "`uname -m`" == "armv6l" ] || [ "`uname -m`" == "aarch64" ] ); then
+if [ "`uname`" != "Darwin" ] && ( [ "`uname -m`" = "armv6l" ] || [ "`uname -m`" = "aarch64" ] ); then
   jplatform="${jplatform:=raspberry}"
-elif [ "`uname`" == "Darwin" ]; then
+elif [ "`uname`" = "Darwin" ]; then
   jplatform="${jplatform:=darwin}"
 else
   jplatform="${jplatform:=linux}"
 fi
 
-if [ "`uname -m`" == "x86_64" ]; then
+if [ "`uname -m`" = "x86_64" ]; then
   j64x="${j64x:=j64avx}"
-elif [ "`uname -m`" == "aarch64" ]; then
+elif [ "`uname -m`" = "aarch64" ]; then
   j64x="${j64x:=j64}"
-elif [ "`uname -m`" == "arm64" ] && [ -z "${jplatform##*darwin*}" ]; then
+elif [ "`uname -m`" = "arm64" ] && [ -z "${jplatform##*darwin*}" ]; then
   j64x="${j64x:=j64arm}"
 else
   j64x="${j64x:=j32}"

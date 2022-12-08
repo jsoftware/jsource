dced768c40027f37c33ea1d44587b5a4ea3032b7
bill lam <bbill.lam@gmail.com>
2022-12-08 10:59:31 +0800

    Revert "test windows g7x6"
    This reverts commit f63d55264a36463bdf0268eaba25b1a85c71bd76.

--- script/buildga.cmd
+++ script/buildga.cmd
@@ -22,6 +22,15 @@ IF %ERRORLEVEL% NEQ 0 EXIT /b %ERRORLEVEL%
 @rem nmake -f makefile.win CC=clang-cl x64=0 USE_PYXES=1
 cd ..
 cd jdll
+nmake -f makefile.win CC=clang-cl x64=1 JAVX512=1 USE_OPENMP=0 USE_PYXES=1 clean
+nmake -f makefile.win CC=clang-cl x64=1 JAVX512=1 USE_OPENMP=0 USE_PYXES=1
+IF %ERRORLEVEL% NEQ 0 EXIT /b %ERRORLEVEL%
+nmake -f makefile.win CC=clang-cl x64=1 JAVX2=1 USE_OPENMP=0 USE_PYXES=1 clean
+nmake -f makefile.win CC=clang-cl x64=1 JAVX2=1 USE_OPENMP=0 USE_PYXES=1
+IF %ERRORLEVEL% NEQ 0 EXIT /b %ERRORLEVEL%
+nmake -f makefile.win CC=clang-cl x64=1 JAVX=1 USE_OPENMP=0 USE_PYXES=1 clean
+nmake -f makefile.win CC=clang-cl x64=1 JAVX=1 USE_OPENMP=0 USE_PYXES=1
+IF %ERRORLEVEL% NEQ 0 EXIT /b %ERRORLEVEL%
 nmake -f makefile.win CC=clang-cl x64=1 USE_OPENMP=0 USE_PYXES=1 clean
 nmake -f makefile.win CC=clang-cl x64=1 USE_OPENMP=0 USE_PYXES=1
 IF %ERRORLEVEL% NEQ 0 EXIT /b %ERRORLEVEL%
--- script/testga.cmd
+++ script/testga.cmd
@@ -4,3 +4,8 @@ dir j64
 
 j64\jconsole -lib j.dll testga.ijs
 IF %ERRORLEVEL% NEQ 0 EXIT /b 1
+j64\jconsole -lib javx.dll testga.ijs
+IF %ERRORLEVEL% NEQ 0 EXIT /b 1
+j64\jconsole -lib javx2.dll testga.ijs
+IF %ERRORLEVEL% NEQ 0 EXIT /b 1
+

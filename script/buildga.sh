1c8e02f06e5c915439e61f5a29a0807d5df3c945
bill lam <bbill.lam@gmail.com>
2022-12-12 10:42:55 +0800

    github action linux debug build

--- script/buildga.sh
+++ script/buildga.sh
@@ -12,8 +12,10 @@ export CC USE_SLEEF _DEBUG
 
 if [ "$1" == "linux" ]; then
   ext="so"
+  export _DEBUG=3
 elif [ "$1" == "raspberry" ]; then
   ext="so"
+  export _DEBUG=3
 elif [ "$1" == "darwin" ]; then
   ext="dylib"
 else

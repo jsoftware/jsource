3a65f63c7516288a7acecd62b28724157f0daa37
bill lam <bbill.lam@gmail.com>
2022-12-12 14:58:39 +0800

    make2/cleanup.sh

--- make2/clean.sh
+++ make2/clean.sh
@@ -21,16 +21,19 @@ find ../jsrc -name "*.o" -type f -delete || true
 find ../dllsrc -name "*.o" -type f -delete || true
 find ../sleef/src -name "*.o" -type f -delete || true
 find ../base64 -name "*.o" -type f -delete || true
+find ../libbacktrace -name "*.o" -type f -delete || true
 find obj -name "*.o" -type f -delete || true
 
 find ../jsrc -name "*.tmp" -type f -delete || true
 find ../dllsrc -name "*.tmp" -type f -delete || true
 find ../sleef/src -name "*.tmp" -type f -delete || true
 find ../base64 -name "*.tmp" -type f -delete || true
+find ../libbacktrace -name "*.tmp" -type f -delete || true
 find obj -name "*.tmp" -type f -delete || true
 
 find ../jsrc -name "*.dSYM" -type d -delete || true
 find ../dllsrc -name "*.dSYM" -type d -delete || true
 find ../sleef/src -name "*.dSYM" -type d -delete || true
 find ../base64 -name "*.dSYM" -type d -delete || true
+find ../libbacktrace -name "*.dSYM" -type d -delete || true
 find obj -name "*.dSYM" -type d -delete || true

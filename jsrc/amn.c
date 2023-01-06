34ac75c007ac7c2d6f0cb082f59f27fe494a81af
Henry Rich <henryhrich@gmail.com>
2023-01-06 15:52:32 -0500

    Commentary for sparse amend bug

--- jsrc/amn.c
+++ jsrc/amn.c
@@ -18,64 +18,69 @@ static I jtcsize(J jt,A z,A ind){B*b;I h,j,m,r,*s;P*zp;
 }    /* data cell size in z for each element of index array ind */
 
 static B jtiaddr(J jt,A z,A ind,A*i1,A*i2){A a,ai,as,ii,jj,q,t,x,y;I c,d,e,h,i,*iv,*jv,m,n,p,*qv,*s,*u,*v,*yv;P*zp;
- zp=PAV(z); a=SPA(zp,a); x=SPA(zp,x); s=1+AS(x); 
- h=AS(ind)[AR(ind)-1];                               /* # axes indexed              */
- RZ(q=gt(sc(h),a)); 
- y=SPA(zp,i); if(!all1(q))RZ(y=repeatr(q,y));          /* indexed cols of index mat   */
- m=AS(y)[0]; yv=AV(y);      
- RZ(ai=IX(h));
- RZ(as=less(IX(AR(z)),a)); u=AV(as); n=AN(as);         /* dense axes                  */
- GATV0(t,INT,n,1); v=AV(t);                             /* shape of indexed dense axes */
- e=0; d=1; DO(n, if(h>u[i])v[e++]=s[i]; else d*=s[i];);
- RZ(*i2=jj=tymes(sc(d),base2(vec(INT,e,v),repeatr(eps(ai,as),ind))));
- c=AS(y)[1];
- if(!c){
-  n=AN(jj);
-  RZ(*i1=repeat(sc(n),IX(m)));
-  RZ(*i2=reshape(sc(m*n),jj));
+ zp=PAV(z); a=SPA(zp,a); x=SPA(zp,x); s=1+AS(x);   // a=sparse axes, x=sparse values, s=->shape of sparse ITEM 
+ h=AS(ind)[AR(ind)-1];   // h=# axes indexed 
+ RZ(q=gt(sc(h),a));   // q=({:$ind) > a, i. e. mask of axes that are indexed by ind
+ y=SPA(zp,i); if(!all1(q))RZ(y=repeatr(q,y));  // y=index table for z, but only the columns indexed by ind
+ m=AS(y)[0]; yv=AV(y);  // m=#sparse items, yv->first sparse-index list (only columns indexed by ind)
+ RZ(ai=IX(h));   // ai=i. #indexed sparse axes
+ RZ(as=less(IX(AR(z)),a)); u=AV(as); n=AN(as);  // as=nonsparse axes of x, n=number of them, u->first
+ GATV0(t,INT,n,1); v=AV(t);    // t=place to build lengths of indexed nonsparse zxes
+ e=0; d=1; DO(n, if(h>u[i])v[e++]=s[i]; else d*=s[i];);  // move the lengths of the indexed nonsparse axes to t; accumulate d=cell-size of nonindexed axes; e=#indexed nonsparse axes
+ RZ(*i2=jj=tymes(sc(d),base2(vec(INT,e,v),repeatr(eps(ai,as),ind))));  // jj= (len of nonindexed part) * (shape of indexed axes) #: (ai e. as) #"_ 1 ind  i. e. offset to the start of the cell in the data area.
+     // note that this cell is noncontiguous: parts of it may be indexed by ind.  We are at the place where all nonindexed axes are 0
+ c=AS(y)[1];  // c=#sparse axes indexed by ind
+ if(!c){   // if no sparse axes indexed by ind...
+  n=AN(jj);  // n=number of rows of ind
+  RZ(*i1=repeat(sc(n),IX(m)));  // i1 = (#ind) # i. #sparse items, i. e. repeat each row of ind withing each sparse cell.  ind is applied within each sparse cell
+  RZ(*i2=reshape(sc(m*n),jj));  // i2 = the offsets to the data area are repeated, one for each row of ind
   R 1;
  }
- RZ(*i1=ii=indexof(y,repeatr(eps(ai,a),ind)));         /* group indices in index mat  */
- if(c==AN(a))R 1;
- n=AN(ii); iv=AV(ii); jv=AV(jj);                       /* do progressive iota         */
- GATV0(q,INT,n,1); qv=AV(q);                            /* group sizes                 */
- for(i=h=0;i<n;++i){
-  e=1; d=iv[i]; v=yv+c*d;
-  DQ(m-d-1, if(ICMP(v,v+c,c))break; ++e; v+=c;);
-  qv[i]=e; h+=e;                                       /* # elements in group i       */
+ RZ(*i1=ii=indexof(y,repeatr(eps(ai,a),ind)));   // ii=y i. (sparse axes of ind), i. e. the cell-indexes of z that are indexed by ind
+ if(c==AN(a))R 1;  // if all sparse axes are indexed by ind, finish, leave i1 and i2 as is
+ n=AN(ii); iv=AV(ii); jv=AV(jj);   // n=#indexed cells in y  iv->index-list of first cell  jv->data offset for each row of ind
+ GATV0(q,INT,n,1); qv=AV(q);    // q=work area, qv->location 0
+ for(i=h=0;i<n;++i){  // for each indexed cell in y...
+  e=1; d=iv[i]; v=yv+c*d;  // d=index in y of row i of ind; v->index list in y
+  DQ(m-d-1, if(ICMP(v,v+c,c))break; ++e; v+=c;);   // count e=consecutive # identical rows of y (#rows of y touched by a single row of ind) 
+  qv[i]=e; h+=e;  // for each row of ind, qv[i] is # rows of y that ind covers; h is the total#
  }
- GATV0(t,INT,h,1); u=AV(t); *i1=t;
+ GATV0(t,INT,h,1); u=AV(t); *i1=t;  // allocate result areas
  GATV0(t,INT,h,1); v=AV(t); *i2=t;
- DO(n, e=qv[i]; d=iv[i]; p=jv[i]; DQ(e, *u++=d++; *v++=p;););
+ DO(n, e=qv[i]; d=iv[i]; p=jv[i]; DQ(e, *u++=d++; *v++=p;););  // for each row of ind, see how many rows of y it touches; copy out the index lists; repeat the start of the data cell
  R 1;
 }    /* index i1 (in index matrix) and address i2 (in data array) from index array */
 
+// ind is array of indexes to cells
 static A jtzpadn(J jt,A z,A ind,B ip){A a,ai,i1,p,p1,q,t,x,x0,y,y0,y1;B*b;I c,d,h,m,n;P*zp;
  // put sparse axi into names a,x,y; remember x&y as x0,y0
- zp=PAV(z); a=SPA(zp,a); x=x0=SPA(zp,x); y=y0=SPA(zp,i);
- n=1; h=AS(ind)[AR(ind)-1];
- RZ(ai=IX(h));
- RZ(t=eps(ai,a)); b=BAV(t); d=0; DO(h, if(b[i])++d;);
- RZ(i1=d<h?repeatr(t,ind):ind); if(2!=AR(ind))RZ(i1=d?reshape(v2(AN(i1)/d,d),i1):mtm);
- RZ(t=gt(sc(h),a)); RZ(y1=all1(t)?y:repeatr(t,y));
- RZ(p=nub(less(i1,y1)));
- if(c=AN(a)-d){
-  RZ(t=from(less(a,ai),shape(z))); RZ(p1=odom(2L,c,AV(t))); n=AS(p1)[0];
-  if(m=AS(p)[0])RZ(p=stitch(repeat(sc(n),p),reshape(v2(n*m,c),p1)));
-  RZ(t=nub(repeat(eps(y1,i1),y1)));
-  RZ(t=stitch(repeat(sc(n),t),reshape(v2(n*AS(t)[0],c),p1)));
-  RZ(t=less(t,y));
-  if(AN(t))RZ(p=over(p,t));
+ zp=PAV(z); a=SPA(zp,a); x=x0=SPA(zp,x); y=y0=SPA(zp,i);  // a=list of sparse axes
+ n=1; h=AS(ind)[AR(ind)-1];  // h=length of last axis of ind = len of frame of addressed cells
+ RZ(ai=IX(h));   // ai = i. {:$ind = axis #s in frame
+ RZ(t=eps(ai,a)); b=BAV(t); d=0; DO(h, if(b[i])++d;);  // t=mask of sparse axes in a that are in frame; d is # of them
+ RZ(i1=d<h?repeatr(t,ind):ind); if(2!=AR(ind))RZ(i1=d?reshape(v2(AN(i1)/d,d),i1):mtm);  // i1 = t#"_ 1 ind, i. e. ind containing only the sparse axes; form into table
+ RZ(t=gt(sc(h),a)); RZ(y1=all1(t)?y:repeatr(t,y));  // y1=table of sparse indexes discarding any axes not in ind
+ RZ(p=nub(less(i1,y1)));  // p=cells indexed by ind that are not already in z
+ if(c=AN(a)-d){   // c=#sparse axes not in the frame, i. e. # sparse axes in the cells.  if there are any...
+  // first, the cells in ind that are entirely missing from y
+  RZ(t=from(less(a,ai),shape(z))); RZ(p1=odom(2L,c,AV(t))); n=AS(p1)[0];  // t=lengths of the axes in the cells; p1=odometer for them; n=total # cells in a modified item
+  if(m=AS(p)[0])RZ(p=stitch(repeat(sc(n),p),reshape(v2(n*m,c),p1)));  // m=#new cells; if there are any, create p = (n # p) ,. ((n*#p) $ p1), i. e. extend p with indexes of sparse axes inside the cells
+  // next, the cells of ind that are already in y, but perhaps partially
+  RZ(t=nub(repeat(eps(y1,i1),y1)));   // t= ~. (y1 e. i1) # y1, i. e. rows of y1 that are already in ind
+  RZ(t=stitch(repeat(sc(n),t),reshape(v2(n*AS(t)[0],c),p1)));  // t = (n # t) ,. (n*#t) $ p1, i. e. extend t with indexes of sparse axes in the cells
+  RZ(t=less(t,y));  // t = t -. y, i. e. discard any cells already in z
+  if(AN(t))RZ(p=over(p,t));  // join the indexes for the full and partially-full new atoms
  }
+// bug: if ind has any nonsparse axis, each list of ind must be filled out with all combinations of the sparse axes not in ind 
  if(m=AS(p)[0]){  /* new cells being added */
-  RZ(y=over(y,p)); RZ(q=grade1(y)); RZ(y=from(q,y));
-  RZ(t=shape(x)); *AV(t)=m; RZ(x=from(q,over(x,reshape(t,SPA(zp,e)))));
+  RZ(y=over(y,p)); RZ(q=grade1(y)); RZ(y=from(q,y));  // y =. y {~ q =. /: y =. y,p  i. e. sort y into cell order, with q the grade vector
+  RZ(t=shape(x)); *AV(t)=m; RZ(x=from(q,over(x,reshape(t,SPA(zp,e)))));   // scaf need mkwris  x =. q { x,sparse ele $~ t =. (#p) 0} $x (x=sparse values)  i. e. x has sparse values insered in new atoms
   // if z is assigned to a name, the use counts need to be adjusted: the old ones need to be decremented
   // to remove the assignment, and the new ones need to be incremented to prevent them from being freed
   // until the name is freed.  We detect the case from jt->zombieval being set to the address of z
   // (if the block could not be inplaced, z will have been changed)
   if(jt->zombieval==z){RZ(ras(y)); RZ(ras(x)); fa(y0); fa(x0);}
-  SPB(zp,i,y); SPB(zp,x,x);
+  SPB(zp,i,y); SPB(zp,x,x);  // install the new indexes and values
  }
  R z;
 }    /* create new indexed cells in z as necessary */

7b623e14fd36a704551f3eda9ecb9cab6d52f917
bill lam <bbill.lam@gmail.com>
2023-01-12 11:00:34 +0800

    test windows qom.exe

--- script/buildga.cmd
+++ script/buildga.cmd
@@ -15,34 +15,34 @@ echo #define jlicense  "commercial" >> jsrc\jversion.h
 echo #define jbuilder  "www.jsoftware.com" >> jsrc\jversion.h
 
 cd makemsvc\jconsole
-..\qom -j2 -f makefile.win CC=clang-cl x64=1 USE_PYXES=1 clean
-..\qom -j2 -f makefile.win CC=clang-cl x64=1 USE_PYXES=1
+..\jom.exe -j2 -f makefile.win CC=clang-cl x64=1 USE_PYXES=1 clean
+..\jom.exe -j2 -f makefile.win CC=clang-cl x64=1 USE_PYXES=1
 IF %ERRORLEVEL% NEQ 0 EXIT /b %ERRORLEVEL%
-@rem ..\qom -j2 -f makefile.win CC=clang-cl x64=0 USE_PYXES=1 clean
-@rem ..\qom -j2 -f makefile.win CC=clang-cl x64=0 USE_PYXES=1
+@rem ..\jom.exe -j2 -f makefile.win CC=clang-cl x64=0 USE_PYXES=1 clean
+@rem ..\jom.exe -j2 -f makefile.win CC=clang-cl x64=0 USE_PYXES=1
 cd ..
 cd jdll
-..\qom -j2 -f makefile.win CC=clang-cl x64=1 JAVX512=1 USE_OPENMP=0 USE_PYXES=1 clean
-..\qom -j2 -f makefile.win CC=clang-cl x64=1 JAVX512=1 USE_OPENMP=0 USE_PYXES=1
+..\jom.exe -j2 -f makefile.win CC=clang-cl x64=1 JAVX512=1 USE_OPENMP=0 USE_PYXES=1 clean
+..\jom.exe -j2 -f makefile.win CC=clang-cl x64=1 JAVX512=1 USE_OPENMP=0 USE_PYXES=1
 IF %ERRORLEVEL% NEQ 0 EXIT /b %ERRORLEVEL%
-..\qom -j2 -f makefile.win CC=clang-cl x64=1 JAVX2=1 USE_OPENMP=0 USE_PYXES=1 clean
-..\qom -j2 -f makefile.win CC=clang-cl x64=1 JAVX2=1 USE_OPENMP=0 USE_PYXES=1
+..\jom.exe -j2 -f makefile.win CC=clang-cl x64=1 JAVX2=1 USE_OPENMP=0 USE_PYXES=1 clean
+..\jom.exe -j2 -f makefile.win CC=clang-cl x64=1 JAVX2=1 USE_OPENMP=0 USE_PYXES=1
 IF %ERRORLEVEL% NEQ 0 EXIT /b %ERRORLEVEL%
-..\qom -j2 -f makefile.win CC=clang-cl x64=1 JAVX=1 USE_OPENMP=0 USE_PYXES=1 clean
-..\qom -j2 -f makefile.win CC=clang-cl x64=1 JAVX=1 USE_OPENMP=0 USE_PYXES=1
+..\jom.exe -j2 -f makefile.win CC=clang-cl x64=1 JAVX=1 USE_OPENMP=0 USE_PYXES=1 clean
+..\jom.exe -j2 -f makefile.win CC=clang-cl x64=1 JAVX=1 USE_OPENMP=0 USE_PYXES=1
 IF %ERRORLEVEL% NEQ 0 EXIT /b %ERRORLEVEL%
-..\qom -j2 -f makefile.win CC=clang-cl x64=1 USE_OPENMP=0 USE_PYXES=1 clean
-..\qom -j2 -f makefile.win CC=clang-cl x64=1 USE_OPENMP=0 USE_PYXES=1
+..\jom.exe -j2 -f makefile.win CC=clang-cl x64=1 USE_OPENMP=0 USE_PYXES=1 clean
+..\jom.exe -j2 -f makefile.win CC=clang-cl x64=1 USE_OPENMP=0 USE_PYXES=1
 IF %ERRORLEVEL% NEQ 0 EXIT /b %ERRORLEVEL%
-@rem ..\qom -j2 -f makefile.win CC=clang-cl x64=0 USE_OPENMP=0 USE_PYXES=1 clean
-@rem ..\qom -j2 -f makefile.win CC=clang-cl x64=0 USE_OPENMP=0 USE_PYXES=1
+@rem ..\jom.exe -j2 -f makefile.win CC=clang-cl x64=0 USE_OPENMP=0 USE_PYXES=1 clean
+@rem ..\jom.exe -j2 -f makefile.win CC=clang-cl x64=0 USE_OPENMP=0 USE_PYXES=1
 cd ..
 cd tsdll
-..\qom -j2 -f makefile.win CC=clang-cl x64=1 clean
-..\qom -j2 -f makefile.win CC=clang-cl x64=1
+..\jom.exe -j2 -f makefile.win CC=clang-cl x64=1 clean
+..\jom.exe -j2 -f makefile.win CC=clang-cl x64=1
 IF %ERRORLEVEL% NEQ 0 EXIT /b %ERRORLEVEL%
-@rem ..\qom -j2 -f makefile.win CC=clang-cl x64=0 clean
-@rem ..\qom -j2 -f makefile.win CC=clang-cl x64=0
+@rem ..\jom.exe -j2 -f makefile.win CC=clang-cl x64=0 clean
+@rem ..\jom.exe -j2 -f makefile.win CC=clang-cl x64=0
 cd ..
 
 copy jconsole\jconsole.exe ..\j64

62babcb341f439702579cafcc434a04328731bd8
bill lam <bbill.lam@gmail.com>
2023-05-01 11:27:14 +0800

    disable strptime test on linux

--- script/install-lnx.sh
+++ script/install-lnx.sh
@@ -3,3 +3,5 @@ sudo dpkg --add-architecture i386
 sudo apt-get install -y build-essential gcc-multilib 
 sudo apt-get install -y libedit-dev libncursesw5-dev
 sudo apt-get install -y nasm
+echo "fr_FR.UTF-8 UTF-8" | sudo tee -a /etc/locale.gen
+sudo locale-gen

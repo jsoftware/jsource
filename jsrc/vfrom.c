40107322f376c26a1373bea52db287ff6198aeda
Moonchild <elronnd@elronnd.net>
2023-06-22 07:40:52 +0000

    beep

--- jsrc/vfrom.c
+++ jsrc/vfrom.c
@@ -473,9 +473,9 @@ A jtcompidx(J jt,I axislen,A ind){
  // Since we expect the count to be small, we allocate a return block of the maximum size.  We then use the tail end
  // to hold a bitmask of the values that have not been crossed off
  RZ(ind=likely(ISDENSETYPE(AT(ind),INT))?ind:cvt(INT,ind));  // ind is now an INT vector, possibly the input argument
- I allolen=1+MAX(axislen-1,1);  // number of words needed.  There must be at least one value crossed off, but we always need at least 1 word for bitmask
-                                // We also reserve one word of buffer between the indices and the bitmask since, in the case of (for example) (<<<_1) { i.65, we could end up overwriting the last word of the bitmask before reading it
-                                // An alternative would be to pipeline the loop, loading the mask for the next iteration before completing the previous iteration, but this would be annoying and bloat
+ I allolen=MAX(axislen,1);  // number of words needed.  There must be at least one value crossed off, but we always need at least 1 word for bitmask
+                            // We also reserve one word of buffer between the indices and the bitmask since, in the case of (for example) (<<<_1) { i.65, we could end up overwriting the last word of the bitmask before reading it
+                            // An alternative would be to pipeline the loop, loading the mask for the next iteration before completing the previous iteration, but this would be annoying and bloat
  A z; GATV0(z,INT,allolen,1) I *zv0=IAV1(z), *zv=zv0;   // allocate the result/temp block.  
  I bwds=(axislen+(BW-1))>>LGBW;  // number of words needed: one bit for each valid index vallue
  I *bv=zv+allolen-bwds; mvc(bwds*SZI,bv,SY_64?4*SZI:2*SZI,validitymask); bv[bwds-1]=~((~1ll)<<((axislen-1)&(BW-1)));  // fill the block with 1s to indicate we need to write; clear ending 0s

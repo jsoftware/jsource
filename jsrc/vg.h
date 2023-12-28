b0dd05404e94960a54519edb7efd44bd4546f011
Henry Rich <henryhrich@gmail.com>
2023-12-28 12:23:56 -0500

    error sorting 2-byte booleans

--- jsrc/vg.h
+++ jsrc/vg.h
@@ -37,19 +37,20 @@ extern I grcol2(I,I,US*,I,I*,I*,const I,US*,I);
 
 extern void msort(SORT *,I,void**,void**,I);
 
-// Convert 2 Booleans to a code 0-3
+// Convert 2 Booleans to a code 0-3.  The input must be construed as bigendian
 #if C_LE
-#define IND2(x) {US xx = (x); ii = ((xx<<9)|xx)>>8;}
+#define IND2(x) {US xx = (x); xx = ((xx<<9)|xx); ii=xx>>8;}
 #else
 #define IND2(x) {US xx = (x); ii = 0x3&((xx>>7)|xx);}
 #endif
 
-// Convert 4 Booleans to a code 0-15
+// Convert 4 Booleans to a code 0-15.  The input must be construed as bigendian
 #if C_LE
 #define IND4(x) {UINT xx = (x); xx|=xx<<9; xx|=xx<<18; ii = xx>>24;}  // first byte (bit 0) is the MSB when a word is loaded
 #else
 #define IND4(x) {UINT xx = (x); ii = 0xf&((xx>>21)|(xx>>14)|(xx>>7)|xx);}
 #endif
+
 
 // endian constants for 16-bit radix sorts
 #define FPLSBWDX (C_LE?0:3)

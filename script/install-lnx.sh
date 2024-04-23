17a52ba7cbe64ddcd74012049549764209a32043
bill lam <bbill.lam@gmail.com>
2024-04-23 09:00:35 +0800

    install script

--- script/install-lnx.sh
+++ script/install-lnx.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 sudo dpkg --add-architecture i386
-sudo apt-get install -y build-essential gcc-multilib g++-multilib libc6-dev libc6-dev-i386
+sudo apt-get update -y
+sudo apt-get upgrade y
+sudo apt-get install -y build-essential gcc-multilib g++-multilib libc6-dev libc6-dev-i386-cross
 sudo apt-get install -y libedit-dev libncursesw5-dev
 sudo apt-get install -y nasm
 echo "fr_FR.UTF-8 UTF-8" | sudo tee -a /etc/locale.gen

12f7251d5844a38d6de4626667095eb3c205e2c9
Henry Rich <henryhrich@gmail.com>
2024-11-03 16:55:36 -0500

    Detect NaN in +/@:*"1!.1

--- jsrc/va2.c
+++ jsrc/va2.c
@@ -1189,7 +1189,7 @@ DF2(jtsumattymes1){
    }
   }
  #endif
-  if(unlikely(NANTEST)){ASSERT(fit=1,EVNAN) RETF(jtsumattymes1(jt,a,w,FAV(self)->fgh[0]))}  // !.1 cannot recover from NaN (which can come from infinities).  Revert !.0 to uncompensated case
+  if(unlikely(NANTEST)){ASSERT(fit==1,EVNAN) RETF(jtsumattymes1(jt,a,w,FAV(self)->fgh[0]))}  // !.1 cannot recover from NaN (which can come from infinities).  Revert !.0 to uncompensated case
  } 
  RETF(z);
 }

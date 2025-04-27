1e1e2d13e885921d84278a9eb54edb83b7e6d592
bill lam <bbill.lam@gmail.com>
2025-04-28 01:14:02 +0800

    github action install libopenblas

--- script/install-lnx.sh
+++ script/install-lnx.sh
@@ -3,7 +3,10 @@ sudo dpkg --add-architecture i386
 sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get install -y build-essential gcc-multilib g++-multilib libc6-dev libc6-dev-i386-cross g++-mingw-w64-x86-64 g++-mingw-w64-i686 autoconf automake libtool
-sudo apt-get install -y nasm libopenblas0-pthread:i386 libopenblas0-pthread libomp-dev
+sudo apt-get install -y nasm libomp-dev
+sudo apt-get search libopenblas0
+sudo apt-get search libomp
+sudo apt-get install -y libopenblas0-pthread
 echo "fr_FR.UTF-8 UTF-8" | sudo tee -a /etc/locale.gen
 sudo locale-gen
 clang --version

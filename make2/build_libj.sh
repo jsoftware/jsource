8bbe6391d3b2bef4220a54324ef87e51ec4038be
bill lam <bbill.lam@gmail.com>
2025-05-03 01:11:09 +0800

    revert tune for gcc older version

--- make2/build_libj.sh
+++ make2/build_libj.sh
@@ -103,6 +103,7 @@ if [ -z "${compiler##*gcc*}" ] || [ -z "${CC##*gcc*}" ]; then
  -Wno-cast-function-type \
  -Wno-clobbered \
  -Wno-empty-body \
+ -Wno-error=stringop-overflow \
  -Wno-format-overflow \
  -Wno-implicit-fallthrough \
  -Wno-incompatible-function-pointer-types \

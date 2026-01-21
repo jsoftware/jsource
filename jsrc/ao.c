4614744136563921f22815c7e7e66a6da3ecafa1
Henry Rich <henryhrich@gmail.com>
2026-01-21 10:08:11 -0500

    MEMAUDIT failure

--- jsrc/ao.c
+++ jsrc/ao.c
@@ -433,7 +433,7 @@ A jtkeyct(J jtfg,A a,A w,A self,D toler){F12IP;PROLOG(0009);A ai,z=0;I nitems;
   // NOT small-range processing: go through the index+size table to create the frets and reordered data for passing to cut
   nfrets=AM(ai);  // fetch # frets before we possibly clone ai
   I maxfretsize=(nitems>>8); maxfretsize=maxfretsize<nfrets?nfrets:maxfretsize; maxfretsize=4*maxfretsize+nfrets+1;  // max # bytes needed for frets, if some are long
-  if((UI)maxfretsize<sizeof(localfrets)-NORMAH*SZI){frets=(A)localfrets; AT(frets)=0; AR(frets)=0; if(MEMAUDIT&0xc)AFLAGFAUX(frets,0)} // Cut tests the type field - only.  If debug, the flag also  rank must be valid in case we call rankex
+  if((UI)maxfretsize<sizeof(localfrets)-NORMAH*SZI){frets=(A)localfrets; AT(frets)=0; AR(frets)=0; if(MEMAUDIT&0x3e)AFLAGFAUX(frets,0)} // Cut tests the type field - only.  If debug, the flag also  rank must be valid in case we call rankex
   // we can write the frets over w if w is inplaceable, DIRECT, has items bigger than an I4, not 32-bit or atom, and not u/..~  w has always been copied to a new buffer by the sort
   else if((likely(w!=a)||likely(FAV(self)->id!=CSLDOTDOT)) && (SZI==8) && likely(AR(w)) && ((((AFLAG(w)&(AFVIRTUAL|AFNJA))-((I)jtfg&JTINPLACEW))&(-(I)(AT(w)&DIRECT))&AC(w)&(4-celllen))<0))frets=w;
   else GATV0(frets,LIT,maxfretsize,0);   // 1 byte per fret is adequate, since we have padding
@@ -479,7 +479,7 @@ A jtkeyct(J jtfg,A a,A w,A self,D toler){F12IP;PROLOG(0009);A ai,z=0;I nitems;
   // now that we have the number of frets, we can allocate the fret block
   I maxfretsize=(nitems>>8); maxfretsize=maxfretsize<nfrets?nfrets:maxfretsize; maxfretsize=4*maxfretsize+nfrets+1;  // max # bytes needed for frets
   // use local fretblock if there are few frets
-  if((UI)maxfretsize<sizeof(localfrets)-NORMAH*SZI){frets=(A)localfrets; AT(frets)=0; AR(frets)=0; if(MEMAUDIT&0xc)AFLAGFAUX(frets,0)} // Cut tests the type field - only; for memaudit we need flag too, and rank in case we rank2ex
+  if((UI)maxfretsize<sizeof(localfrets)-NORMAH*SZI){frets=(A)localfrets; AT(frets)=0; AR(frets)=0; if(MEMAUDIT&0x3e)AFLAGFAUX(frets,0)} // Cut tests the type field - only; for memaudit we need flag too, and rank in case we rank2ex
   // we can write the frets over w if w is inplaceable, DIRECT, has items bigger than an I4, not 32-bit or atom, and not u/..~  w has always been copied to a new buffer by the sort
   else if((likely(w!=a)||likely(FAV(self)->id!=CSLDOTDOT)) && (SZI==8) && likely(AR(w)) && ((((AFLAG(w)&(AFVIRTUAL|AFNJA))-((I)jtfg&JTINPLACEW))&(-(I)(AT(w)&DIRECT))&AC(w)&(4-celllen))<0))frets=w;
   else GATV0(frets,LIT,maxfretsize,0);   // 1 byte per fret is adequate, since we have padding

61e9e87c396e3895ffabf64bf56b30dc4b6316fe
bill lam <bbill.lam@gmail.com>
2026-02-02 14:48:22 +0800

    ip

--- script/install-rpi.sh
+++ script/install-rpi.sh
@@ -2,10 +2,13 @@
 sudo dpkg --add-architecture armhf
 sudo apt-get update -y
 sudo apt-get upgrade -y
-sudo apt-get install -y build-essential gcc-multilib libc6-dev autoconf automake libtool gdb gdb-multiarch
-sudo apt-get install -y nasm libomp5-14 libopenblas0-pthread
+sudo apt-get install -y build-essential libc6-dev autoconf automake libtool
+sudo apt-get install -y libtool gdb gdb-multiarch
+sudo apt-get install -y nasm libomp5-14 libopenblas0-pthread gdb-arm-linux-gnueabihf
 sudo apt-get install -y linux-libc-dev-armhf-cross libc6-dev-armhf-cross libc6-armhf-cross gcc-arm-linux-gnueabihf
 sudo apt-get install -y libc6:armhf libstdc++6:armhf libc6-dev:armhf
+sudo apt-get install -y ibomp5-14:armhf
+sudo apt-get install -y libopenblas0-pthread:armhf
 echo "fr_FR.UTF-8 UTF-8" | sudo tee -a /etc/locale.gen
 sudo locale-gen
 clang --version

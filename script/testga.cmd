ce73fdfb678d4e6ea4d33f0c6c72aa5d2b329dd0
bill lam <bbill.lam@gmail.com>
2026-02-09 20:59:30 +0800

    github workflow

--- script/testga.cmd
+++ script/testga.cmd
@@ -17,10 +17,10 @@ IF "%~1"=="x86" GOTO L01A
 IF "%~1"=="arm64" GOTO L01B
 IF "%~1" NEQ "x64" EXIT /b 1
 @rem copy "C:\Program Files\LLVM\bin\liblldb.dll" "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Tools\Llvm\x64\bin\"
-curl --output-dir "C:/Program Files/Microsoft Visual Studio/2022/Enterprise/VC/Tools/Llvm/x64/bin/" -o "liblldb.dll" "https://www.jsoftware.com/download/winlib/x64/liblldb.dll"
-curl --output-dir "C:/Program Files/Microsoft Visual Studio/2022/Enterprise/VC/Tools/Llvm/x64/bin/" -o "lldb.exe" "https://www.jsoftware.com/download/winlib/x64/lldb.exe"
-"C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Tools\Llvm\x64\bin\lldb" --version
-lldb --version
+@rem curl --output-dir "C:/Program Files/Microsoft Visual Studio/2022/Enterprise/VC/Tools/Llvm/x64/bin/" -o "liblldb.dll" "https://www.jsoftware.com/download/winlib/x64/liblldb.dll"
+@rem curl --output-dir "C:/Program Files/Microsoft Visual Studio/2022/Enterprise/VC/Tools/Llvm/x64/bin/" -o "lldb.exe" "https://www.jsoftware.com/download/winlib/x64/lldb.exe"
+@rem "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Tools\Llvm\x64\bin\lldb" --version
+@rem lldb --version
 
 dir j64
 IF "%USE_EMU_AVX%"=="0" GOTO L01F

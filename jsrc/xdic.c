282ed7172dd0fe3834238a38426f1633f228b3d5
bill lam <bbill.lam@gmail.com>
2026-02-11 18:21:12 +0800

    raspi xdic ip2

--- jsrc/xdic.c
+++ jsrc/xdic.c
@@ -1705,16 +1705,16 @@ static INLINE UI8 jtdelslotso(DIC *dic,void *k,I n,J jt,UI lv,VIRT virt,B *zv){I
     sibling=RDIR(parent,parentd^1);   // Fetch new sibling, which is perforce black
    }
    // Here sibling is black (it may have been a same-child, now swapped into that position)
-#if 0 && defined(RASPI)
-  {
-fprintf(stderr,">>> sibling "FMTI" nodeb "FMTI" nodeb>>24 "FMTI" \n",sibling, nodeb, (nodeb>>24));
-fprintf(stderr,">>> hashtbl[..] "FMTI" \n",(sibling)*(nodeb>>24));
-fprintf(stderr,">>> hashtbl[] %p \n",(hashtbl+(sibling)*(nodeb>>24)));
-siblingch=*(UI8*)&hashtbl[(sibling)*(nodeb>>24)];
-I ss=(parentd)?(C)nodeb:0, os=(parentd)?0:(C)nodeb;
-siblings=(siblingch>>ss)&_bzhi_u64(~(UI8)1,nodeb);
-siblingo=(siblingch>>os)&_bzhi_u64(~(UI8)1,nodeb); siblingc=siblingch&1;
-}
+#if defined(RASPI)
+   {
+// fprintf(stderr,">>> sibling "FMTI" nodeb "FMTI" nodeb>>24 "FMTI" \n",sibling, nodeb, (nodeb>>24));
+// fprintf(stderr,">>> hashtbl[..] "FMTI" \n",(sibling)*(nodeb>>24));
+// fprintf(stderr,">>> hashtbl[] %p \n",(hashtbl+(sibling)*(nodeb>>24)));
+   siblingch=*(UI8*)&hashtbl[(sibling)*(nodeb>>24)];
+   I ss=(parentd)?(C)nodeb:0, os=(parentd)?0:(C)nodeb;
+   siblings=(siblingch>>ss)&_bzhi_u64(~(UI8)1,nodeb);
+   siblingo=(siblingch>>os)&_bzhi_u64(~(UI8)1,nodeb); siblingc=siblingch&1;
+   }
 #else
    RSOC(sibling,sibling,parentd)  //  fetch sibling's children, as same/opp
 #endif
